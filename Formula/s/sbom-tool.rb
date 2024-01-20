973f8d392228fa84df3bec84f4656b726dd90d8a
Timothy Sutton <tim@synthist.net>
2024-01-19 21:58:08 -0500

    sbom-tool 2.2.3

--- Formula/s/sbom-tool.rb
+++ Formula/s/sbom-tool.rb
@@ -1,8 +1,8 @@
 class SbomTool < Formula
   desc "Scalable and enterprise ready tool to create SBOMs for any variety of artifacts"
   homepage "https://github.com/microsoft/sbom-tool"
-  url "https://github.com/microsoft/sbom-tool/archive/refs/tags/v2.2.2.tar.gz"
-  sha256 "c6ee17a84d1e16cf1ee81d6b8ed9f5ab80c35317a2f4bdc9cc7360f88248c9dd"
+  url "https://github.com/microsoft/sbom-tool/archive/refs/tags/v2.2.3.tar.gz"
+  sha256 "9f2a42835aa664ec71f22d6ad5c9be9a83b5938167c3798ebab8c844c4d57996"
   license "MIT"
   head "https://github.com/microsoft/sbom-tool.git", branch: "main"
 
